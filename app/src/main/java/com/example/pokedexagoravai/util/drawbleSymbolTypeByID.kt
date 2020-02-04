17ce6dd763bbec84884bf3faa78033e52ad64454
Eduardo Dornel <eduardo.ribas@taglivros.com.br>
2020-02-04 09:57:42 -0300

    feat(detail):adicionar imagem com simbolo do tipo ao lado dos itens de lista de tipos.

---
+++ app/src/main/java/com/example/pokedexagoravai/util/drawbleSymbolTypeByID.kt
@@ -0,0 +1,24 @@
+package com.example.symbol_pokedexagoravai.util
+
+import com.example.pokedexagoravai.R
+
+val drawableSymbolTypeByID = mapOf(
+    "Fire" to R.drawable.symbol_fire,
+    "Water" to R.drawable.symbol_water,
+    "Grass" to R.drawable.symbol_grass,
+    "Electric" to R.drawable.symbol_eletric,
+    "Fighting" to R.drawable.symbol_fighting,
+    "Ice" to R.drawable.symbol_ice,
+    "Psychic" to R.drawable.symbol_psychic,
+    "Poison"  to R.drawable.symbol_poison,
+    "Normal" to R.drawable.symbol_normal,
+    "Dark" to R.drawable.symbol_dark,
+    "Ground" to R.drawable.symbol_ground,
+    "Steel" to R.drawable.symbol_steel,
+    "Rock" to R.drawable.symbol_rock,
+    "Bug" to R.drawable.symbol_bug,
+    "Flying" to R.drawable.symbol_flying,
+    "Ghost" to R.drawable.symbol_ghost,
+    "Dragon" to R.drawable.symbol_dragon,
+    "Fairy"  to R.drawable.symbol_fairy
+)
--- app/src/main/java/com/example/pokedexagoravai/view/PokeTypesVyewHolder.kt
+++ app/src/main/java/com/example/pokedexagoravai/view/PokeTypesVyewHolder.kt
@@ -4,15 +4,19 @@ import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pokedexagoravai.util.colorTypeByID
+import com.example.symbol_pokedexagoravai.util.drawableSymbolTypeByID
 import kotlinx.android.synthetic.main.item_type.view.*
 
 class PokeTypesVyewHolder (itemView : View) :RecyclerView.ViewHolder(itemView){
 
     val typeText = itemView.text_type_1
+    val imageType = itemView.image_type
 
     fun bind(type:String){
         println("ninja $type")
         typeText.text = type
         typeText.setTextColor(ContextCompat.getColor(itemView.context, colorTypeByID.get(type)?:0))
+        imageType.setImageDrawable(ContextCompat.getDrawable(itemView.context,drawableSymbolTypeByID.get(type)?:0))
+        imageType.visibility = View.VISIBLE
     }
 }
