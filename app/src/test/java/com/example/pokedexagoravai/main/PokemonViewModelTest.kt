5ae4948d152a1ad8fcaf7a0dc81fadcf20177216
Jhonata Avila <joavilati@gmail.com.br>
2020-10-09 22:06:28 -0300

    feat(PokemonViewModelTest): testar falso positivo.

--- app/src/test/java/com/example/pokedexagoravai/main/PokemonViewModelTest.kt
+++ app/src/test/java/com/example/pokedexagoravai/main/PokemonViewModelTest.kt
@@ -34,6 +34,7 @@ class PokemonViewModelTest: BaseTest() {
             readJSON<List<Pokemon>>("PokemonsListJSON")
         )
         val expectedValue = Assertion(listPokemon)
+        val notExpectedValue = Assertion(listPokemon2)
         `when`(
             viewModel.repository.service.getPokemons()
         ).thenReturn(response)
@@ -41,7 +42,9 @@ class PokemonViewModelTest: BaseTest() {
         viewModel.getPokemons()
 
         expectedValue isEqualTo viewModel.pokemons.getOrAwaitValue()
+        notExpectedValue isDifferentFrom viewModel.pokemons.getOrAwaitValue()
     }
+
 }
 val listPokemon = listOf<Pokemon>(
     Pokemon(
@@ -92,6 +95,50 @@ val listPokemon = listOf<Pokemon>(
         xdescription = "There is a large flower on Venusaur's back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower's aroma soothes the emotions of people.",
         category = "Seed",
         typeofpokemon = arrayListOf("Grass", "Poison"),
+        weaknesses = arrayListOf(
+            "Fire",
+            "Flying",
+            "Ice",
+            "Psychic"
+        ),
+        evolutions = arrayListOf(
+            "#001",
+            "#002",
+            "#003"
+        )
+    )
+)
+
+val listPokemon2 = listOf<Pokemon>(
+    Pokemon(
+        name = "Bulbasaur",
+        id = "#001",
+        imageurl = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+        xdescription = "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.",
+        category = "Seed",
+        typeofpokemon = arrayListOf("Grass", "Poison"),
+        weaknesses = arrayListOf(
+            "Fire",
+            "Flying",
+            "Ice",
+            "Psychic"
+        ),
+        evolutions = arrayListOf(
+            "#001",
+            "#002",
+            "#003"
+        )
+    ),
+    Pokemon(
+        name = "Ivysaur",
+        id = "#002",
+        imageurl = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png",
+        xdescription = "There is a bud on this Pok\u00e9mon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon.",
+        category = "Seed",
+        typeofpokemon = arrayListOf(
+            "Grass",
+            "Poison"
+        ),
         weaknesses = arrayListOf(
             "Fire",
             "Flying",
